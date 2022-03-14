5041a25b1235b0513369f9439ab407a593cfa9b9
Vera Harless <53271741+morningvera@users.noreply.github.com>
2022-03-14 10:10:27 -0400

    remove unnecessary const

--- rest/aggs/aggs.go
+++ rest/aggs/aggs.go
@@ -9,37 +9,34 @@ import (
 
 // todo: add comments for godoc
 
-const (
-	GetPath               = "/v2/aggs/ticker/{ticker}/range/{multiplier}/{resolution}/{from}/{to}"
-	GetPreviousClosePath  = "/v2/aggs/ticker/{ticker}/prev"
-	GetGroupedDailyPath   = "/v2/aggs/grouped/locale/{locale}/market/{marketType}/{date}"
-	GetDailyOpenClosePath = "/v1/open-close/{ticker}/{date}"
-)
-
 type Client struct {
 	client.BaseClient
 }
 
 func (ac *Client) Get(ctx context.Context, params GetParams, opts ...client.Option) (*AggsResponse, error) {
 	res := &AggsResponse{}
-	err := ac.Call(http.MethodGet, GetPath, params, res, append([]client.Option{client.WithContext(ctx)}, opts...)...)
+	path := "/v2/aggs/ticker/{ticker}/range/{multiplier}/{resolution}/{from}/{to}"
+	err := ac.Call(http.MethodGet, path, params, res, append([]client.Option{client.WithContext(ctx)}, opts...)...)
 	return res, err
 }
 
 func (ac *Client) GetPreviousClose(ctx context.Context, params GetPreviousCloseParams, opts ...client.Option) (*AggsResponse, error) {
 	res := &AggsResponse{}
-	err := ac.Call(http.MethodGet, GetPreviousClosePath, params, res, append([]client.Option{client.WithContext(ctx)}, opts...)...)
+	path := "/v2/aggs/ticker/{ticker}/prev"
+	err := ac.Call(http.MethodGet, path, params, res, append([]client.Option{client.WithContext(ctx)}, opts...)...)
 	return res, err
 }
 
 func (ac *Client) GetGroupedDaily(ctx context.Context, params GetGroupedDailyParams, opts ...client.Option) (*AggsResponse, error) {
 	res := &AggsResponse{}
-	err := ac.Call(http.MethodGet, GetGroupedDailyPath, params, res, append([]client.Option{client.WithContext(ctx)}, opts...)...)
+	path := "/v2/aggs/grouped/locale/{locale}/market/{marketType}/{date}"
+	err := ac.Call(http.MethodGet, path, params, res, append([]client.Option{client.WithContext(ctx)}, opts...)...)
 	return res, err
 }
 
 func (ac *Client) GetDailyOpenClose(ctx context.Context, params GetDailyOpenCloseParams, opts ...client.Option) (*DailyOpenCloseResponse, error) {
 	res := &DailyOpenCloseResponse{}
-	err := ac.Call(http.MethodGet, GetDailyOpenClosePath, params, res, append([]client.Option{client.WithContext(ctx)}, opts...)...)
+	path := "/v1/open-close/{ticker}/{date}"
+	err := ac.Call(http.MethodGet, path, params, res, append([]client.Option{client.WithContext(ctx)}, opts...)...)
 	return res, err
 }
