e8bc320089db48a7c49424d785c6e52e1b3c91ae
Vera Harless <53271741+morningvera@users.noreply.github.com>
2022-03-15 12:53:31 -0400

    add client type and implement aggs client (#7)
    Co-authored-by: mcdayoub <38268139+mcdayoub@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/polygon-io/client-golang
 
-go 1.13
+go 1.17
+
+require (
+	github.com/go-resty/resty/v2 v2.7.0
+	github.com/gorilla/websocket v1.5.0
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ rest/aggs/models.go
@@ -0,0 +1,234 @@
+package aggs
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/polygon-io/client-golang/rest/client"
+)
+
+// Aggregate is an aggregation of all the activity on a specified ticker between the start and end timestamps.
+// For more details see https://polygon.io/docs/stocks/get_v2_aggs_ticker__stocksticker__range__multiplier___timespan___from___to.
+type Aggregate struct {
+	Ticker            string  `json:"T,omitempty"`
+	Volume            float64 `json:"v"`
+	VWAP              float64 `json:"vw,omitempty"`
+	AggregateVWAP     float64 `json:"a,omitempty"`
+	Open              float64 `json:"o"`
+	Close             float64 `json:"c"`
+	High              float64 `json:"h"`
+	Low               float64 `json:"l"`
+	Timestamp         int64   `json:"t"`
+	Transactions      int64   `json:"n,omitempty"`
+	Market            string  `json:"m,omitempty"`
+	Exchange          int32   `json:"x,omitempty"`
+	Locale            string  `json:"g,omitempty"`
+	OfficialOpenPrice float64 `json:"op,omitempty"`
+	AverageSize       float64 `json:"z,omitempty"`
+	AccumulatedVolume float64 `json:"av,omitempty"`
+	StartTimestamp    int64   `json:"s,omitempty"`
+	EndTimestamp      int64   `json:"e,omitempty"`
+}
+
+// AggsResponse is returned by the aggs API. It contains a list of aggregates for the specified ticker.
+type AggsResponse struct {
+	client.BaseResponse
+	Ticker       string      `json:"ticker,omitempty"`
+	QueryCount   int         `json:"queryCount"`
+	ResultsCount int         `json:"resultsCount"`
+	Adjusted     bool        `json:"adjusted"`
+	Aggs         []Aggregate `json:"results,omitempty"`
+}
+
+// Sort the results by timestamp. asc will return results in ascending order (oldest at the top),
+// desc will return results in descending order (newest at the top).
+type Sort string
+
+const (
+	Asc  Sort = "asc"
+	Desc Sort = "desc"
+)
+
+// Resolution is the size of the time window.
+type Resolution string
+
+const (
+	Minute  Resolution = "minute"
+	Hour    Resolution = "hour"
+	Day     Resolution = "day"
+	Week    Resolution = "week"
+	Month   Resolution = "month"
+	Quarter Resolution = "quarter"
+	Year    Resolution = "year"
+)
+
+// GetParams is the set of path and query parameters that can be used when requesting aggs through the Get method.
+type GetParams struct {
+	Ticker      string
+	Multiplier  int
+	Resolution  Resolution
+	From        time.Time
+	To          time.Time
+	QueryParams GetQueryParams
+}
+
+// GetQueryParams is the set of query parameters that can be used when requesting aggs through the Get method.
+type GetQueryParams struct {
+	Sort     *Sort
+	Limit    *int32
+	Adjusted *bool
+	Explain  *bool
+}
+
+// Path maps the input Get parameters to their respective keys.
+func (p GetParams) Path() map[string]string {
+	return map[string]string{
+		"ticker":     p.Ticker,
+		"multiplier": fmt.Sprint(p.Multiplier),
+		"resolution": fmt.Sprint(p.Resolution),
+		"from":       fmt.Sprint(p.From.UnixMilli()),
+		"to":         fmt.Sprint(p.To.UnixMilli()),
+	}
+}
+
+// Query maps the input Get parameters to their respective keys.
+func (p GetParams) Query() map[string]string {
+	q := map[string]string{}
+
+	if p.QueryParams.Sort != nil {
+		q["sort"] = string(*p.QueryParams.Sort)
+	}
+
+	if p.QueryParams.Limit != nil {
+		q["limit"] = strconv.FormatInt(int64(*p.QueryParams.Limit), 10)
+	}
+
+	if p.QueryParams.Adjusted != nil {
+		q["adjusted"] = strconv.FormatBool(*p.QueryParams.Adjusted)
+	}
+
+	if p.QueryParams.Explain != nil {
+		q["explain"] = strconv.FormatBool(*p.QueryParams.Explain)
+	}
+
+	return q
+}
+
+// GetPreviousCloseParams is the set of path and query parameters that can be used when requesting aggs through the GetPreviousClose method.
+type GetPreviousCloseParams struct {
+	Ticker      string
+	QueryParams GetPreviousCloseQueryParams
+}
+
+// GetPreviousCloseQueryParams is the set of query parameters that can be used when requesting aggs through the GetPreviousClose method.
+type GetPreviousCloseQueryParams struct {
+	Adjusted *bool
+}
+
+// Path maps the GetPreviousCloseParams path parameters to their respective keys.
+func (p GetPreviousCloseParams) Path() map[string]string {
+	return map[string]string{
+		"ticker": p.Ticker,
+	}
+}
+
+// Query maps the GetPreviousCloseParams query parameters to their respective keys.
+func (p GetPreviousCloseParams) Query() map[string]string {
+	q := map[string]string{}
+
+	if p.QueryParams.Adjusted != nil {
+		q["adjusted"] = strconv.FormatBool(*p.QueryParams.Adjusted)
+	}
+
+	return q
+}
+
+// MarketType is the type of market used to query aggs using the GetGroupedDaily method.
+type MarketType string
+
+const (
+	Stocks MarketType = "stocks"
+	Forex  MarketType = "forex"
+	Crypto MarketType = "crypto"
+)
+
+// GetGroupedDailyParams is the set of path and query parameters that can be used when requesting aggs through the GetGroupedDaily method.
+type GetGroupedDailyParams struct {
+	Locale      string
+	MarketType  MarketType
+	Date        time.Time
+	QueryParams GetGroupedDailyQueryParams
+}
+
+// GetGroupedDailyQueryParams is the set of query parameters that can be used when requesting aggs through the GetGroupedDaily method.
+type GetGroupedDailyQueryParams struct {
+	Adjusted *bool
+}
+
+// Path maps the GetGroupedDaily path parameters to their respective keys.
+func (p GetGroupedDailyParams) Path() map[string]string {
+	return map[string]string{
+		"locale":     p.Locale,
+		"marketType": string(p.MarketType),
+		"date":       p.Date.Format("2006-01-02"),
+	}
+}
+
+// Query maps the GetGroupedDaily query parameters to their respective keys.
+func (p GetGroupedDailyParams) Query() map[string]string {
+	q := map[string]string{}
+
+	if p.QueryParams.Adjusted != nil {
+		q["adjusted"] = strconv.FormatBool(*p.QueryParams.Adjusted)
+	}
+
+	return q
+}
+
+// DailyOpenCloseResponse is the response for the DailyOpenClose method.
+// Get the open, close and afterhours prices of a stock symbol on a certain date.
+// For more details see https://polygon.io/docs/stocks/get_v1_open-close__stocksticker___date.
+type DailyOpenCloseResponse struct {
+	client.BaseResponse
+	Symbol     string  `json:"symbol"`
+	From       string  `json:"from"`
+	Open       float64 `json:"open"`
+	High       float64 `json:"high"`
+	Low        float64 `json:"low"`
+	Close      float64 `json:"close"`
+	Volume     float64 `json:"volume"`
+	AfterHours float64 `json:"afterHours"`
+	PreMarket  float64 `json:"preMarket"`
+}
+
+// GetDailyOpenCloseParams is the set of path and query parameters that can be used when requesting aggs through the GetDailyOpenClose method.
+type GetDailyOpenCloseParams struct {
+	Ticker      string
+	Date        time.Time
+	QueryParams GetDailyOpenCloseQueryParams
+}
+
+// GetDailyOpenCloseQueryParams is the set of query parameters that can be used when requesting aggs through the GetDailyOpenCloseQuery method.
+type GetDailyOpenCloseQueryParams struct {
+	Adjusted *bool
+}
+
+// Path maps the GetDailyOpenClose path parameters to their respective keys.
+func (p GetDailyOpenCloseParams) Path() map[string]string {
+	return map[string]string{
+		"ticker": p.Ticker,
+		"date":   p.Date.Format("2006-01-02"),
+	}
+}
+
+// Query maps the GetDailyOpenClose query parameters to their respective keys.
+func (p GetDailyOpenCloseParams) Query() map[string]string {
+	q := map[string]string{}
+
+	if p.QueryParams.Adjusted != nil {
+		q["adjusted"] = strconv.FormatBool(*p.QueryParams.Adjusted)
+	}
+
+	return q
+}
