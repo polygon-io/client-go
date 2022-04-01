399c6f005ae7647eac13cbc79c7548245558efcc
Vera Harless <53271741+morningvera@users.noreply.github.com>
2022-04-01 15:25:39 -0400

    update aggs tests

--- rest/models/aggs.go
+++ rest/models/aggs.go
@@ -29,9 +29,9 @@ type GetAggsParams struct {
 type GetAggsResponse struct {
 	BaseResponse
 	Ticker       string `json:"ticker,omitempty"`
-	QueryCount   int    `json:"queryCount"`
-	ResultsCount int    `json:"resultsCount"`
-	Adjusted     bool   `json:"adjusted"`
+	QueryCount   int    `json:"queryCount,omitempty"`
+	ResultsCount int    `json:"resultsCount,omitempty"`
+	Adjusted     bool   `json:"adjusted,omitempty"`
 	Results      []Agg  `json:"results,omitempty"`
 }
 
@@ -48,9 +48,9 @@ type GetGroupedDailyAggsParams struct {
 type GetGroupedDailyAggsResponse struct {
 	BaseResponse
 	Ticker       string `json:"ticker,omitempty"`
-	QueryCount   int    `json:"queryCount"`
-	ResultsCount int    `json:"resultsCount"`
-	Adjusted     bool   `json:"adjusted"`
+	QueryCount   int    `json:"queryCount,omitempty"`
+	ResultsCount int    `json:"resultsCount,omitempty"`
+	Adjusted     bool   `json:"adjusted,omitempty"`
 	Results      []Agg  `json:"results,omitempty"`
 }
 
@@ -65,15 +65,15 @@ type GetDailyOpenCloseAggParams struct {
 // GetDailyOpenCloseAggResponse is the response for the GetDailyOpenCloseAgg method.
 type GetDailyOpenCloseAggResponse struct {
 	BaseResponse
-	Symbol     string  `json:"symbol"`
-	From       string  `json:"from"`
-	Open       float64 `json:"open"`
-	High       float64 `json:"high"`
-	Low        float64 `json:"low"`
-	Close      float64 `json:"close"`
-	Volume     float64 `json:"volume"`
-	AfterHours float64 `json:"afterHours"`
-	PreMarket  float64 `json:"preMarket"`
+	Symbol     string  `json:"symbol,omitempty"`
+	From       string  `json:"from,omitempty"`
+	Open       float64 `json:"open,omitempty"`
+	High       float64 `json:"high,omitempty"`
+	Low        float64 `json:"low,omitempty"`
+	Close      float64 `json:"close,omitempty"`
+	Volume     float64 `json:"volume,omitempty"`
+	AfterHours float64 `json:"afterHours,omitempty"`
+	PreMarket  float64 `json:"preMarket,omitempty"`
 }
 
 // GetPreviousCloseAggParams is the set of parameters for the GetPreviousCloseAgg method.
@@ -87,23 +87,23 @@ type GetPreviousCloseAggParams struct {
 type GetPreviousCloseAggResponse struct {
 	BaseResponse
 	Ticker       string `json:"ticker,omitempty"`
-	QueryCount   int    `json:"queryCount"`
-	ResultsCount int    `json:"resultsCount"`
-	Adjusted     bool   `json:"adjusted"`
+	QueryCount   int    `json:"queryCount,omitempty"`
+	ResultsCount int    `json:"resultsCount,omitempty"`
+	Adjusted     bool   `json:"adjusted,omitempty"`
 	Results      []Agg  `json:"results,omitempty"`
 }
 
 // Agg is an aggregation of all the activity on a specified ticker between the start and end timestamps.
 type Agg struct {
 	Ticker            string  `json:"T,omitempty"`
-	Volume            float64 `json:"v"`
+	Volume            float64 `json:"v,omitempty"`
 	VWAP              float64 `json:"vw,omitempty"`
 	AggregateVWAP     float64 `json:"a,omitempty"`
-	Open              float64 `json:"o"`
-	Close             float64 `json:"c"`
-	High              float64 `json:"h"`
-	Low               float64 `json:"l"`
-	Timestamp         int64   `json:"t"`
+	Open              float64 `json:"o,omitempty"`
+	Close             float64 `json:"c,omitempty"`
+	High              float64 `json:"h,omitempty"`
+	Low               float64 `json:"l,omitempty"`
+	Timestamp         int64   `json:"t,omitempty"`
 	Transactions      int64   `json:"n,omitempty"`
 	Market            string  `json:"m,omitempty"`
 	Exchange          int32   `json:"x,omitempty"`
