f9e50b4eef141cd054c7797bab9f44813605988a
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-06 15:10:42 -0400

    added marketstatus to reference

--- rest/reference/market.go
+++ rest/reference/market.go
@@ -14,3 +14,11 @@ func (c *Client) GetMarketHolidays(ctx context.Context, params models.GetMarketH
 	err := c.Call(ctx, http.MethodGet, models.GetMarketHolidaysPath, params, res, options...)
 	return res, err
 }
+
+// GetMarketStatus retrieves the current trading status of the exchanges and overall financial markets.
+// For more details see https://polygon.io/docs/stocks/get_v1_marketstatus_now.
+func (c *Client) GetMarketStatus(ctx context.Context, params models.GetMarketStatusParams, options ...models.RequestOption) (*models.GetMarketStatusResponse, error) {
+	res := &models.GetMarketStatusResponse{}
+	err := c.Call(ctx, http.MethodGet, models.GetMarketStatusPath, params, res, options...)
+	return res, err
+}
