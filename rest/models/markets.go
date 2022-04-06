8d75df6b90dc4d65f7d13e151f47be7a7a71b6e6
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-06 15:06:35 -0400

    added marketstatus

--- rest/models/markets.go
+++ rest/models/markets.go
@@ -2,6 +2,7 @@ package models
 
 const (
 	GetMarketHolidaysPath = "/v1/marketstatus/upcoming"
+	GetMarketStatusPath   = "/v1/marketstatus/now"
 )
 
 // todo: this endpoint is unlikely to ever have params so should we delete this type?
@@ -11,6 +12,13 @@ type GetMarketHolidaysParams struct{}
 // GetMarketHolidaysResponse is the response returned by the GetMarketHolidays method.
 type GetMarketHolidaysResponse []MarketHoliday
 
+// todo: this endpoint is unlikely to ever have params so should we delete this type?
+// GetMarketStatusParams is the set of parameters for the GetMarketStatus method.
+type GetMarketStatusParams struct{}
+
+// GetMarketStatussResponse is the response returned by the GetMarketStats method.
+type GetMarketStatusResponse MarketStatus
+
 // MarketHoliday represents a market holiday for a specific exchange.
 type MarketHoliday struct {
 	Exchange string `json:"exchange,omitempty"`
@@ -19,4 +27,14 @@ type MarketHoliday struct {
 	Status   string `json:"status,omitempty"`
 	Open     string `json:"open,omitempty"`  // todo: "2006-01-02T00:00:00.000Z" format
 	Close    string `json:"close,omitempty"` // todo: "2006-01-02T00:00:00.000Z" format
+}
+
+// MarketStatus represents the current trading status of the exchanges and overall financial markets.
+type MarketStatus struct {
+	AfterHours bool              `json:"afterHours,omitempty"`
+	Currencies map[string]string `json:"currencies,omitempty"`
+	EarlyHours bool              `json:"earlyHours,omitempty"`
+	Exchanges  map[string]string `json:"exchanges,omitempty"`
+	Market     string            `json:"market,omitempty"`
+	ServerTime string            `json:"serverTime,omitempty"` // todo: "2006-01-02T00:00:00.000Z" format
 }
