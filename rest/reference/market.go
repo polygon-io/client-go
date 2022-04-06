2e6006aa944683ede4f6ad9bb606bab57de5819a
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-06 16:51:23 -0400

    removed params

--- rest/reference/market.go
+++ rest/reference/market.go
@@ -9,16 +9,16 @@ import (
 
 // GetMarketHolidays retrieves upcoming market holidays and their open/close times.
 // For more details see https://polygon.io/docs/stocks/get_v1_marketstatus_upcoming.
-func (c *Client) GetMarketHolidays(ctx context.Context, params models.GetMarketHolidaysParams, options ...models.RequestOption) (*models.GetMarketHolidaysResponse, error) {
+func (c *Client) GetMarketHolidays(ctx context.Context, options ...models.RequestOption) (*models.GetMarketHolidaysResponse, error) {
 	res := &models.GetMarketHolidaysResponse{}
-	err := c.Call(ctx, http.MethodGet, models.GetMarketHolidaysPath, params, res, options...)
+	err := c.Call(ctx, http.MethodGet, models.GetMarketHolidaysPath, nil, res, options...)
 	return res, err
 }
 
 // GetMarketStatus retrieves the current trading status of the exchanges and overall financial markets.
 // For more details see https://polygon.io/docs/stocks/get_v1_marketstatus_now.
-func (c *Client) GetMarketStatus(ctx context.Context, params models.GetMarketStatusParams, options ...models.RequestOption) (*models.GetMarketStatusResponse, error) {
+func (c *Client) GetMarketStatus(ctx context.Context, options ...models.RequestOption) (*models.GetMarketStatusResponse, error) {
 	res := &models.GetMarketStatusResponse{}
-	err := c.Call(ctx, http.MethodGet, models.GetMarketStatusPath, params, res, options...)
+	err := c.Call(ctx, http.MethodGet, models.GetMarketStatusPath, nil, res, options...)
 	return res, err
 }
