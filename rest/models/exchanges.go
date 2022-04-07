aba5f78484138d59eb0acbe23b6c55755df574e9
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-07 15:01:49 -0400

    add exchanges model

---
+++ rest/models/exchanges.go
@@ -0,0 +1,27 @@
+package models
+
+const ListExchangesPath = "/v3/reference/exchanges"
+
+// ListExchangesParams is the set of parameters for the ListExchanges method.
+type ListExchangesParams struct {
+	AssetClass *string `query:"asset_class,omitempty"` // todo: enum value
+	Locale     *string `query:"locale,omitempty"`      // todo: enum value
+}
+
+// ListExchangesResponse is the response returned by the ListExchanges method.
+type ListExchangesResponse struct {
+	BaseResponse
+	Results []Exchange `json:"results,omitempty"`
+}
+
+// Exchange contains detailed information on a specified stock Exchange.
+type Exchange struct {
+	Acronym       string `json:"acronym,omitempty"`
+	AssetClass    string `json:"asset_class,omitempty"`
+	ID            int64  `json:"id,omitempty"`
+	Locale        string `json:"locale,omitempty"`
+	MIC           string `json:"mic,omitempty"`
+	ParticipantID string `json:"participant_id,omitempty"`
+	Type          string `json:"type,omitempty"`
+	URL           string `json:"url,omitempty"`
+}
