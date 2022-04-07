3282040225fd35077b5d92bae02d8eca131efe8d
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-07 11:38:27 -0400

    fixed naming conventions

--- rest/models/splits.go
+++ rest/models/splits.go
@@ -4,17 +4,17 @@ const ListSplitsPath = "/v3/reference/splits"
 
 // ListSplitsParams is the set of parameters for the ListSplits method.
 type ListSplitsParams struct {
-	Ticker    *string `query:"ticker"`
+	TickerEQ  *string `query:"ticker"`
 	TickerLT  *string `query:"ticker.lt"`
 	TickerLTE *string `query:"ticker.lte"`
 	TickerGT  *string `query:"ticker.gt"`
 	TickerGTE *string `query:"ticker.gte"`
 
-	ExeDate    *string `query:"execution_date"` // todo: this is "YYYY-MM-DD" format, need to figure out the best way to encode this without interfering with the default
-	ExeDateLT  *string `query:"execution_date.lt"`
-	ExeDateLTE *string `query:"execution_date.lte"`
-	ExeDateGT  *string `query:"execution_date.gt"`
-	ExeDateGTE *string `query:"execution_date.gte"`
+	ExecutionDateEQ  *string `query:"execution_date"` // todo: this is "YYYY-MM-DD" format, need to figure out the best way to encode this without interfering with the default
+	ExecutionDateLT  *string `query:"execution_date.lt"`
+	ExecutionDateLTE *string `query:"execution_date.lte"`
+	ExecutionDateGT  *string `query:"execution_date.gt"`
+	ExecutionDateGTE *string `query:"execution_date.gte"`
 
 	ReverseSplit *bool `query:"reverse_split"`
 
@@ -31,8 +31,8 @@ type ListSplitsResponse struct {
 
 // Split contains detailed information on a specified stock split.
 type Split struct {
-	ExeDate string `json:"execution_date,omitempty"`
-	From    int64  `json:"split_from,omitempty"`
-	To      int64  `json:"split_to,omitempty"`
-	Ticker  string `json:"ticker,omitempty"`
+	ExecutionDate string `json:"execution_date,omitempty"`
+	SplitFrom     int64  `json:"split_from,omitempty"`
+	SplitTo       int64  `json:"split_to,omitempty"`
+	Ticker        string `json:"ticker,omitempty"`
 }
