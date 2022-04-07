2436aa3e6b7489e1e45f6562679b5b89579dfea0
Darcy Linde <47221647+Darcy-Linde@users.noreply.github.com>
2022-04-07 15:50:37 -0400

    Darcy.exchanges (#45)

---
+++ rest/models/exchanges.go
@@ -0,0 +1,29 @@
+package models
+
+const GetExchangesPath = "/v3/reference/exchanges"
+
+// GetExchangesParams is the set of parameters for the GetExchanges method.
+type GetExchangesParams struct {
+	AssetClass *string `query:"asset_class,omitempty"` // todo: enum value
+	Locale     *string `query:"locale,omitempty"`      // todo: enum value
+}
+
+// GetExchangesResponse is the response returned by the GetExchanges method.
+type GetExchangesResponse struct {
+	BaseResponse
+	Results []Exchange `json:"results,omitempty"`
+}
+
+// Exchange contains detailed information on a specified stock Exchange.
+type Exchange struct {
+	Acronym       string `json:"acronym,omitempty"`
+	AssetClass    string `json:"asset_class,omitempty"`
+	ID            int64  `json:"id,omitempty"`
+	Locale        string `json:"locale,omitempty"`
+	MIC           string `json:"mic,omitempty"`
+	Name          string `json:"name,omitempty"`
+	OperatingMIC  string `json:"operating_mic,omitempty"`
+	ParticipantID string `json:"participant_id,omitempty"`
+	Type          string `json:"type,omitempty"`
+	URL           string `json:"url,omitempty"`
+}
---
+++ rest/reference/exchanges.go
@@ -0,0 +1,15 @@
+package reference
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+// GetExchanges lists all exchanges that Polygon.io knows about.
+func (c *Client) GetExchanges(ctx context.Context, params models.GetExchangesParams, options ...models.RequestOption) (*models.GetExchangesResponse, error) {
+	res := &models.GetExchangesResponse{}
+	err := c.Call(ctx, http.MethodGet, models.GetExchangesPath, params, res, options...)
+	return res, err
+}
