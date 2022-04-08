b5f2dede3e783bb4bf073ba63df4dfa6c66ce747
mcdayoub <38268139+mcdayoub@users.noreply.github.com>
2022-04-07 21:09:04 -0400

    quotes

--- rest/models/quotes.go
+++ rest/models/quotes.go
@@ -1,9 +1,5 @@
 package models
 
-import (
-	"time"
-)
-
 const (
 	ListQuotesPath        = "/v3/quotes/{ticker}"
 	GetLastQuotePath      = "/v2/last/nbbo/{ticker}"
@@ -14,14 +10,14 @@ const (
 type ListQuotesParams struct {
 	Ticker string `validate:"required" path:"ticker"`
 
-	TimestampEQ  *time.Time `query:"timestamp"`
-	TimestampLT  *time.Time `query:"timestamp.lt"`
-	TimestampLTE *time.Time `query:"timestamp.lte"`
-	TimestampGT  *time.Time `query:"timestamp.gt"`
-	TimestampGTE *time.Time `query:"timestamp.gte"`
-	Order        *Order     `query:"order"`
-	Limit        *int       `query:"limit"`
-	Sort         *Sort      `query:"sort"`
+	TimestampEQ  *Nanos `query:"timestamp"`
+	TimestampLT  *Nanos `query:"timestamp.lt"`
+	TimestampLTE *Nanos `query:"timestamp.lte"`
+	TimestampGT  *Nanos `query:"timestamp.gt"`
+	TimestampGTE *Nanos `query:"timestamp.gte"`
+	Order        *Order `query:"order"`
+	Limit        *int   `query:"limit"`
+	Sort         *Sort  `query:"sort"`
 }
 
 // ListQuotesResponse is the response returned by the ListQuotes method.
@@ -64,11 +60,11 @@ type Quote struct {
 	BidSize              float64 `json:"bid_size,omitempty"`
 	Conditions           []int32 `json:"conditions,omitempty"`
 	Indicators           []int32 `json:"indicators,omitempty"`
-	ParticipantTimestamp int64   `json:"participant_timestamp,omitempty"`
-	SequenceNumber       int64   `json:"sequence_number,omitempty"`
-	SipTimestamp         int64   `json:"sip_timestamp,omitempty"`
+	ParticipantTimestamp *Nanos  `json:"participant_timestamp,omitempty"`
+	SequenceNumber       *Nanos  `json:"sequence_number,omitempty"`
+	SipTimestamp         *Nanos  `json:"sip_timestamp,omitempty"`
 	Tape                 int32   `json:"tape,omitempty"`
-	TrfTimestamp         int64   `json:"trf_timestamp,omitempty"`
+	TrfTimestamp         *Nanos  `json:"trf_timestamp,omitempty"`
 }
 
 // LastQuote is the most recent NBBO for a ticker symbol.
@@ -82,11 +78,11 @@ type LastQuote struct {
 	BidSize              float64 `json:"s,omitempty"`
 	Conditions           []int32 `json:"c,omitempty"`
 	Indicators           []int32 `json:"i,omitempty"`
-	ParticipantTimestamp int64   `json:"y,omitempty"`
-	SequenceNumber       int64   `json:"q,omitempty"`
-	SipTimestamp         int64   `json:"t,omitempty"`
+	ParticipantTimestamp *Nanos  `json:"y,omitempty"`
+	SequenceNumber       *Nanos  `json:"q,omitempty"`
+	SipTimestamp         *Nanos  `json:"t,omitempty"`
 	Tape                 int32   `json:"z,omitempty"`
-	TrfTimestamp         int64   `json:"f,omitempty"`
+	TrfTimestamp         *Nanos  `json:"f,omitempty"`
 }
 
 // ForexQuote is a BBO for a forex currency pair.
@@ -94,5 +90,5 @@ type ForexQuote struct {
 	Ask       float64 `json:"ask,omitempty"`
 	Bid       float64 `json:"bid,omitempty"`
 	Exchange  int     `json:"exchange,omitempty"`
-	Timestamp int64   `json:"timestamp,omitempty"`
+	Timestamp *Nanos  `json:"timestamp,omitempty"`
 }
