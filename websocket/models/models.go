9898bcf107aabd8f54d671b90eed658a5d5a68b3
Darcy Linde <47221647+Darcy-Linde@users.noreply.github.com>
2022-04-20 10:20:09 -0400

    Darcy.handle data and models (#79)

--- websocket/models/models.go
+++ websocket/models/models.go
@@ -50,6 +50,7 @@ type CurrencyAgg struct {
 	VWAP           float64 `json:"vw,omitempty"`
 	StartTimestamp int64   `json:"s,omitempty"`
 	EndTimestamp   int64   `json:"e,omitempty"`
+	AVGTradeSize   int32   `json:"z,omitempty"`
 }
 
 type EquityTrade struct {
@@ -65,7 +66,7 @@ type EquityTrade struct {
 	SequenceNumber int64   `json:"q,omitempty"`
 }
 
-type CurrencyTrade struct {
+type CryptoTrade struct {
 	EventType
 	Symbol            string  `json:"sym,omitempty"`
 	Exchange          int32   `json:"x,omitempty"`
@@ -75,4 +76,74 @@ type CurrencyTrade struct {
 	Conditions        []int32 `json:"c,omitempty"`
 	Timestamp         int64   `json:"t,omitempty"`
 	ReceivedTimestamp int64   `json:"r,omitempty"`
+}
+
+type EquityQuote struct {
+	EventType
+	Symbol         string  `json:"sym,omitempty"`
+	BidExchangeID  int32   `json:"bx,omitempty"`
+	BidPrice       float64 `json:"bp,omitempty"`
+	BidSize        int32   `json:"bs,omitempty"`
+	AskExchangeID  int32   `json:"ax,omitempty"`
+	AskPrice       float64 `json:"ap,omitempty"`
+	AskSize        int32   `json:"as,omitempty"`
+	Condition      int32   `json:"c,omitempty"`
+	Timestamp      int64   `json:"t,omitempty"`
+	Tape           int32   `json:"z,omitempty"`
+	SequenceNumber int64   `json:"q,omitempty"`
+}
+
+type ForexQuote struct {
+	EventType
+	Pair       string  `json:"p,omitempty"`
+	ExchangeID int32   `json:"x,omitempty"`
+	AskPrice   float64 `json:"a,omitempty"`
+	BidPrice   float64 `json:"b,omitempty"`
+	Timestamp  int64   `json:"t,omitempty"`
+}
+
+type CryptoQuote struct {
+	EventType
+	Pair              string  `json:"pair,omitempty"`
+	BidPrice          float64 `json:"bp,omitempty"`
+	BidSize           int32   `json:"bs,omitempty"`
+	AskPrice          float64 `json:"ap,omitempty"`
+	AskSize           int32   `json:"as,omitempty"`
+	Timestamp         int64   `json:"t,omitempty"`
+	ExchangeID        int32   `json:"x,omitempty"`
+	ReceivedTimestamp int64   `json:"r,omitempty"`
+}
+
+type Imbalance struct {
+	EventType
+	Symbol            string  `json:"T,omitempty"`
+	Timestamp         int64   `json:"t,omitempty"`
+	AuctionTime       int32   `json:"at,omitempty"`
+	AuctionType       string  `json:"a,omitempty"`
+	SymbolSequence    int32   `json:"i,omitempty"`
+	ExchangeID        int32   `json:"x,omitempty"`
+	ImbalanceQuantity int32   `json:"o,omitempty"`
+	PairedQuantity    int32   `json:"p,omitempty"`
+	BookClearingPrice float64 `json:"b,omitempty"`
+}
+
+type LimitUpLimitDown struct {
+	EventType
+	Symbol         string  `json:"T,omitempty"`
+	HighPrice      float64 `json:"h,omitempty"`
+	LowPrice       float64 `json:"l,omitempty"`
+	Indicators     []int32 `json:"i,omitempty"`
+	Tape           int32   `json:"z,omitempty"`
+	Timestamp      int64   `json:"t,omitempty"`
+	SequenceNumber int64   `json:"q,omitempty"`
+}
+
+type Level2Book struct {
+	EventType
+	Pair              string    `json:"pair,omitempty"`
+	BidPrices         []float64 `json:"b,omitempty"`
+	AskPrices         []float64 `json:"a,omitempty"`
+	Timestamp         int64     `json:"t,omitempty"`
+	ExchangeID        int32     `json:"x,omitempty"`
+	ReceivedTimestamp int64     `json:"r,omitempty"`
 }
