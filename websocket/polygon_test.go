88f38a946bc6314a4bc07258f1c017ca268ada65
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-21 10:55:21 -0400

    replace Topic typecasting with existing topic variables

--- websocket/polygon_test.go
+++ websocket/polygon_test.go
@@ -8,20 +8,20 @@ import (
 )
 
 func TestSupportsTopic(t *testing.T) {
-	assert.Equal(t, true, supportsTopic("stocks", Topic(12)))
-	assert.Equal(t, false, supportsTopic("stocks", Topic(40)))
-	assert.Equal(t, true, supportsTopic("options", Topic(31)))
-	assert.Equal(t, false, supportsTopic("options", Topic(40)))
-	assert.Equal(t, true, supportsTopic("forex", Topic(52)))
-	assert.Equal(t, false, supportsTopic("forex", Topic(40)))
-	assert.Equal(t, true, supportsTopic("crypto", Topic(74)))
-	assert.Equal(t, false, supportsTopic("crypto", Topic(40)))
-	assert.Equal(t, false, supportsTopic("fakeMarket", Topic(15)))
+	assert.Equal(t, true, supportsTopic("stocks", StocksMinAggs))
+	assert.Equal(t, false, supportsTopic("stocks", stocksMax))
+	assert.Equal(t, true, supportsTopic("options", OptionsSecAggs))
+	assert.Equal(t, false, supportsTopic("options", StocksMinAggs))
+	assert.Equal(t, true, supportsTopic("forex", ForexQuotes))
+	assert.Equal(t, false, supportsTopic("forex", OptionsQuotes))
+	assert.Equal(t, true, supportsTopic("crypto", CryptoL2Book))
+	assert.Equal(t, false, supportsTopic("crypto", cryptoMin))
+	assert.Equal(t, false, supportsTopic("testMarket", StocksImbalances))
 }
 
 func TestGetParams(t *testing.T) {
-	p1, _ := getParams("stocks", Topic(12), "AAPL", "GME", "HOOD")
-	p2, _ := getParams("stocks", Topic(12))
+	p1, _ := getParams("stocks", StocksMinAggs, "AAPL", "GME", "HOOD")
+	p2, _ := getParams("stocks", StocksMinAggs)
 	s1 := "AM.AAPL,AM.GME,AM.HOOD"
 	s2 := "AM.*"
 	assert.Equal(t, p1, s1)
@@ -36,22 +36,25 @@ func TestSubscriptions(t *testing.T) {
 		Log:       logrus.New(),
 	})
 
-	c.setSubscriptions(Topic(12), "AAPL", "TSLA")
+	c.setSubscriptions(StocksMinAggs, "AAPL", "TSLA")
 	_, aapl := c.subscriptions["AM"]["AAPL"]
 	_, tsla := c.subscriptions["AM"]["TSLA"]
 	assert.Equal(t, true, aapl)
 	assert.Equal(t, true, tsla)
-	c.deleteSubscriptions(Topic(12), "AAPL", "NFLX")
+	c.deleteSubscriptions(StocksMinAggs, "AAPL", "NFLX")
 	_, aapl = c.subscriptions["AM"]["AAPL"]
 	assert.Equal(t, false, aapl)
-	c.setSubscriptions(Topic(12), "*")
+	c.setSubscriptions(StocksMinAggs, "*")
 	_, all := c.subscriptions["AM"]["*"]
 	_, tsla = c.subscriptions["AM"]["TSLA"]
 	assert.Equal(t, false, tsla)
 	assert.Equal(t, true, all)
+	c.deleteSubscriptions(StocksMinAggs, "*")
+	_, all = c.subscriptions["AM"]["*"]
+	assert.Equal(t, false, all)
 	_, trade := c.subscriptions["T"]
 	assert.Equal(t, false, trade)
-	c.deleteSubscriptions(Topic(13), "RDFN")
+	c.deleteSubscriptions(StocksTrades, "RDFN")
 	_, trade = c.subscriptions["T"]
 	assert.Equal(t, true, trade)
 }
