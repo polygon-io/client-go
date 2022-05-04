efec727c73b7fb4b4938e18de061090a41990369
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-05-04 13:56:33 -0400

    change interface to struct

--- rest/models/financials.go
+++ rest/models/financials.go
@@ -119,16 +119,16 @@ type ListStockFinancialsResponse struct {
 
 // StockFinancial contains detailed information on a specified stock financial.
 type StockFinancial struct {
-	CIK                 string                 `json:"cik,omitempty"`
-	CompanyName         string                 `json:"company_name,omitempty"`
-	EndDate             string                 `json:"end_date,omitempty"`
-	FilingDate          string                 `json:"filing_date,omitempty"`
-	Financials          map[string]interface{} `json:"financials,omitempty"`
-	FiscalPeriod        string                 `json:"fiscal_period,omitempty"`
-	FiscalYear          string                 `json:"fiscal_year,omitempty"`
-	SourceFilingFileUrl string                 `json:"source_filing_file_url,omitempty"`
-	SourceFilingUrl     string                 `json:"source_filing_url,omitempty"`
-	StartDate           string                 `json:"start_date,omitempty"`
+	CIK                 string                          `json:"cik,omitempty"`
+	CompanyName         string                          `json:"company_name,omitempty"`
+	EndDate             string                          `json:"end_date,omitempty"`
+	FilingDate          string                          `json:"filing_date,omitempty"`
+	Financials          map[string]map[string]DataPoint `json:"financials,omitempty"`
+	FiscalPeriod        string                          `json:"fiscal_period,omitempty"`
+	FiscalYear          string                          `json:"fiscal_year,omitempty"`
+	SourceFilingFileUrl string                          `json:"source_filing_file_url,omitempty"`
+	SourceFilingUrl     string                          `json:"source_filing_url,omitempty"`
+	StartDate           string                          `json:"start_date,omitempty"`
 }
 
 type DataPoint struct {
