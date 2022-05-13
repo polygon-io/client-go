c9bb98a4980acdfa8ee843b09770915d6d153851
Vera Harless <53271741+morningvera@users.noreply.github.com>
2022-05-13 15:22:18 -0400

    document reference tickers models (#114)

--- rest/models/response.go
+++ rest/models/response.go
@@ -2,21 +2,31 @@ package models
 
 import "fmt"
 
-// BaseResponse has all possible attributes that any response can use. It's intended to be embedded in a
-// domain specific response struct.
+// BaseResponse has all possible attributes that any response can use. It's intended to be embedded in a domain specific
+// response struct.
 type BaseResponse struct {
-	Status       string `json:"status,omitempty"`
-	RequestID    string `json:"request_id,omitempty"`
-	Count        int    `json:"count,omitempty"`
-	Message      string `json:"message,omitempty"`
-	ErrorMessage string `json:"error,omitempty"`
-
 	PaginationHooks
+
+	// The status of this request's response.
+	Status string `json:"status,omitempty"`
+
+	// A request id assigned by the server.
+	RequestID string `json:"request_id,omitempty"`
+
+	// The total number of results for this request.
+	Count int `json:"count,omitempty"`
+
+	// A response message for successful requests.
+	Message string `json:"message,omitempty"`
+
+	// An error message for unsuccessful requests.
+	ErrorMessage string `json:"error,omitempty"`
 }
 
-// PaginationHooks are links to next and/or previous pages. Embed this struct into an API response if
-// the endpoint supports pagination.
+// PaginationHooks are links to next and/or previous pages. Embed this struct into an API response if the endpoint
+// supports pagination.
 type PaginationHooks struct {
+	// If present, this value can be used to fetch the next page of data.
 	NextURL string `json:"next_url,omitempty"`
 }
 
@@ -26,8 +36,10 @@ func (p PaginationHooks) NextPage() string {
 
 // ErrorResponse represents an API response with an error status code.
 type ErrorResponse struct {
-	StatusCode int
 	BaseResponse
+
+	// An HTTP status code for unsuccessful requests.
+	StatusCode int
 }
 
 // Error returns the details of an error response.
