d51dba67d16ce5d6f98a3bf6667fca876eb698ba
Darcy Linde <47221647+Darcy-Linde@users.noreply.github.com>
2022-05-13 14:01:57 -0400

    Rest Example based on README (#113)

---
+++ rest/example/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	polygon "github.com/polygon-io/client-go/rest"
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func main() {
+	c := polygon.New(os.Getenv("POLYGON_API_KEY"))
+
+	params := models.GetAllTickersSnapshotParams{
+		Locale:     models.US,
+		MarketType: models.Stocks,
+	}.WithTickers("AAPL,MSFT")
+
+	res, err := c.GetAllTickersSnapshot(context.Background(), params)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(res) // do something with the result
+}
