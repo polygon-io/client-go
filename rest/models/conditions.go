696a77e3932702ea3effc6b290b8facaebe6025e
Vera Harless <53271741+morningvera@users.noreply.github.com>
2022-05-16 10:22:39 -0400

    revert consolidated types

--- rest/models/conditions.go
+++ rest/models/conditions.go
@@ -67,30 +67,36 @@ type ListConditionsResponse struct {
 
 // Condition contains detailed information on a specified condition.
 type Condition struct {
-	Abbreviation string   `json:"abbreviation,omitempty"`
-	AssetClass   string   `json:"asset_class,omitempty"`
-	DataTypes    []string `json:"data_types,omitempty"`
-	Description  string   `json:"description,omitempty"`
-	Exchange     int64    `json:"exchange,omitempty"`
-	ID           int64    `json:"id,omitempty"`
-	Legacy       bool     `json:"legacy"`
-	Name         string   `json:"name,omitempty"`
-	SIPMapping   struct {
-		CTA  string `json:"CTA,omitempty"`
-		OPRA string `json:"OPRA,omitempty"`
-		UTP  string `json:"UTP,omitempty"`
-	} `json:"sip_mapping,omitempty"`
-	Type        string `json:"type,omitempty"`
-	UpdateRules struct {
-		Consolidated struct {
-			UpdatesHighLow   bool `json:"updates_high_low,omitempty"`
-			UpdatesOpenClose bool `json:"updates_open_close,omitempty"`
-			UpdatesVolume    bool `json:"updates_volume,omitempty"`
-		} `json:"consolidated,omitempty"`
-		MarketCenter struct {
-			UpdatesHighLow   bool `json:"updates_high_low,omitempty"`
-			UpdatesOpenClose bool `json:"updates_open_close,omitempty"`
-			UpdatesVolume    bool `json:"updates_volume,omitempty"`
-		} `json:"market_center,omitempty"`
-	} `json:"update_rules,omitempty"`
+	Abbreviation string      `json:"abbreviation,omitempty"`
+	AssetClass   string      `json:"asset_class,omitempty"`
+	DataTypes    []string    `json:"data_types,omitempty"`
+	Description  string      `json:"description,omitempty"`
+	Exchange     int64       `json:"exchange,omitempty"`
+	ID           int64       `json:"id,omitempty"`
+	Legacy       bool        `json:"legacy"`
+	Name         string      `json:"name,omitempty"`
+	SIPMapping   SIPMapping  `json:"sip_mapping,omitempty"`
+	Type         string      `json:"type,omitempty"`
+	UpdateRules  UpdateRules `json:"update_rules,omitempty"`
+}
+
+// SIPMapping maps a condition to symbols for each SIP.
+type SIPMapping struct {
+	CTA  string `json:"CTA,omitempty"`
+	OPRA string `json:"OPRA,omitempty"`
+	UTP  string `json:"UTP,omitempty"`
+}
+
+// UpdateRules is a list of aggregation rules.
+type UpdateRules struct {
+	Consolidated struct {
+		UpdatesHighLow   bool `json:"updates_high_low,omitempty"`
+		UpdatesOpenClose bool `json:"updates_open_close,omitempty"`
+		UpdatesVolume    bool `json:"updates_volume,omitempty"`
+	} `json:"consolidated,omitempty"`
+	MarketCenter struct {
+		UpdatesHighLow   bool `json:"updates_high_low,omitempty"`
+		UpdatesOpenClose bool `json:"updates_open_close,omitempty"`
+		UpdatesVolume    bool `json:"updates_volume,omitempty"`
+	} `json:"market_center,omitempty"`
 }
