ba278484e2f4232ba0d3c4f1e17e121013748b7d
jbonzo <8647805+jbonzo@users.noreply.github.com>
2022-11-29 15:26:24 -0500

    Launchpad required headers

--- rest/models/request.go
+++ rest/models/request.go
@@ -48,3 +48,25 @@ func QueryParam(key, value string) RequestOption {
 		o.QueryParams.Add(key, value)
 	}
 }
+
+// Headers required to use the Launchpad product.
+const (
+	HeaderEdgeID = "X-Polygon-Edge-ID"
+	HeaderEdgeIPAddress = "X-Polygon-Edge-IP-Address "
+	HeaderEdgeUserAgent = "X-Polygon-Edge-User-Agent"
+)
+
+// EdgeID sets the Launchpad required header denoting the Edge User's ID.
+func EdgeID(id string) RequestOption {
+	return Header(HeaderEdgeID, id)
+}
+
+// EdgeIPAddress sets the Launchpad required header denoting the Edge User's IP Address.
+func EdgeIPAddress(ip string) RequestOption {
+	return Header(HeaderEdgeIPAddress, ip)
+}
+
+// EdgeUserAgent sets the Launchpad required header denoting the Edge User's UserAgent.
+func EdgeUserAgent(userAgent string) RequestOption {
+	return Header(HeaderEdgeUserAgent, userAgent)
+}
