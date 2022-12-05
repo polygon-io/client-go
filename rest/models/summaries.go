915296c4d9d7da79d863fcbcca5f38db8fb5885e
chaig15 <7276836+chaig15@users.noreply.github.com>
2022-12-05 11:22:29 -0800

    fix date

--- rest/models/summaries.go
+++ rest/models/summaries.go
@@ -1,9 +1,6 @@
 package models
 
-import (
-	"github.com/polygon-io/ptime"
-	"strings"
-)
+import "strings"
 
 type GetSummaryParams struct {
 	// The ticker list to get summaries for
@@ -49,10 +46,10 @@ type Session struct {
 
 //easyjson:json
 type Option struct {
-	ContractType      string                       `json:"contract_type,omitempty"`
-	ExerciseStyle     string                       `json:"exercise_style,omitempty"`
-	ExpirationDate    ptime.IDaysPolygonDateString `json:"expiration_date,omitempty"`
-	SharesPerContract float64                      `json:"shares_per_contract,omitempty"`
-	StrikePrice       float64                      `json:"strike_price,omitempty"`
-	UnderlyingTicker  string                       `json:"underlying_ticker,omitempty"`
+	ContractType      string  `json:"contract_type,omitempty"`
+	ExerciseStyle     string  `json:"exercise_style,omitempty"`
+	ExpirationDate    Date    `json:"expiration_date,omitempty"`
+	SharesPerContract float64 `json:"shares_per_contract,omitempty"`
+	StrikePrice       float64 `json:"strike_price,omitempty"`
+	UnderlyingTicker  string  `json:"underlying_ticker,omitempty"`
 }
