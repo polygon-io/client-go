02f048d1de51a8b4da3a76912b29129020bf977a
Ricky Barillas <8647805+jbonzo@users.noreply.github.com>
2023-01-10 10:29:33 -0500

    Version 1.8.0 (#228)

--- rest/client/client.go
+++ rest/client/client.go
@@ -12,7 +12,7 @@ import (
 	"github.com/polygon-io/client-go/rest/models"
 )
 
-const clientVersion = "v1.7.0"
+const clientVersion = "v1.8.0"
 
 const (
 	APIURL            = "https://api.polygon.io"
