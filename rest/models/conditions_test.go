889a662d6457aa27ea4e23d33630a1ec9ec9ac0c
Katie Adams <katie@polygon.io>
2023-04-04 16:28:16 -0400

    A few tests to start with

---
+++ rest/models/conditions_test.go
@@ -0,0 +1,29 @@
+package models_test
+
+import (
+	"testing"
+
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func TestListConditionsParams(t *testing.T) {
+	assetClass := models.AssetClass("Equity")
+	dataType := models.DataType("EndOfDay")
+	id := int64(1)
+	sip := models.SIP("CTA")
+	order := models.Order("asc")
+	limit := 20
+	sort := models.Sort("name")
+
+	expect := models.ListConditionsParams{
+		AssetClass: &assetClass,
+		DataType:   &dataType,
+		ID:         &id,
+		SIP:        &sip,
+		Order:      &order,
+		Limit:      &limit,
+		Sort:       &sort,
+	}
+	actual := models.ListConditionsParams{}.WithAssetClass(assetClass).WithDataType(dataType).WithID(id).WithSIP(sip).WithOrder(order).WithLimit(limit).WithSort(sort)
+	checkParams(t, expect, *actual)
+}
