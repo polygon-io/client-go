8dcba6a144fbb462740080e47ceffaa372e87bab
lukeoleson <29187677+lukeoleson@users.noreply.github.com>
2023-05-22 08:28:26 -0700

    Added examples and updated models for forex and crypto for new v3 snapshot endpoint (#290)

--- rest/models/snapshot.go
+++ rest/models/snapshot.go
@@ -438,23 +438,24 @@ type ListUniversalSnapshotsResponse struct {
 
 // SnapshotResponseModel contains all the information that might come back in a SnapshotResponse.
 type SnapshotResponseModel struct {
-	Name         string            `json:"name,omitempty"`
-	MarketStatus string            `json:"market_status,omitempty"`
-	Ticker       string            `json:"ticker,omitempty"`
-	Type         string            `json:"type,omitempty"`
-	LastQuote    SnapshotLastQuote `json:"last_quote,omitempty"`
-	LastTrade    SnapshotLastTrade `json:"last_trade,omitempty"`
-	Session      Session           `json:"session,omitempty"`
-
-	BreakEvenPrice    float64         `json:"break_even_price,omitempty"`
-	Details           Details         `json:"details,omitempty"`
-	Greeks            Greeks          `json:"greeks,omitempty"`
-	ImpliedVolatility float64         `json:"implied_volatility,omitempty"`
-	OpenInterest      float64         `json:"open_interest,omitempty"`
-	UnderlyingAsset   UnderlyingAsset `json:"underlying_asset,omitempty"`
-
-	Error   string `json:"error"`
-	Message string `json:"message"`
+	Name              string            `json:"name,omitempty"`
+	MarketStatus      string            `json:"market_status,omitempty"`
+	Ticker            string            `json:"ticker,omitempty"`
+	Type              string            `json:"type,omitempty"`
+	LastQuote         SnapshotLastQuote `json:"last_quote,omitempty"`
+	LastTrade         SnapshotLastTrade `json:"last_trade,omitempty"`
+	Session           Session           `json:"session,omitempty"`
+	BreakEvenPrice    float64           `json:"break_even_price,omitempty"`
+	Details           Details           `json:"details,omitempty"`
+	Greeks            Greeks            `json:"greeks,omitempty"`
+	ImpliedVolatility float64           `json:"implied_volatility,omitempty"`
+	OpenInterest      float64           `json:"open_interest,omitempty"`
+	UnderlyingAsset   UnderlyingAsset   `json:"underlying_asset,omitempty"`
+	Value             float64           `json:"value,omitempty"`
+	LastUpdated       int64             `json:"last_updated,omitempty"`
+	Timeframe         string            `json:"timeframe,omitempty"`
+	Error             string            `json:"error"`
+	Message           string            `json:"message"`
 }
 
 // SnapshotLastQuote contains all the information that might come back in the last_quote attribute of a SnapshotResponse.
@@ -466,18 +467,20 @@ type SnapshotLastQuote struct {
 	LastUpdated int64   `json:"last_updated,omitempty"`
 	Midpoint    float64 `json:"midpoint,omitempty"`
 	Timeframe   string  `json:"timeframe,omitempty"`
+	Exchange    int64   `json:"exchange,omitempty"`
 }
 
 // SnapshotLastTrade contains all the information that might come back in the last_trade attribute of a SnapshotResponse.
 type SnapshotLastTrade struct {
-	Timestamp   int64   `json:"sip_timestamp,omitempty"`
-	Conditions  []int32 `json:"conditions,omitempty"`
-	Price       float64 `json:"price,omitempty"`
-	Size        uint32  `json:"size,omitempty"`
-	Exchange    int32   `json:"exchange,omitempty"`
-	Timeframe   string  `json:"timeframe,omitempty"`
-	ID          string  `json:"id,omitempty"`
-	LastUpdated int64   `json:"last_updated,omitempty"`
+	Timestamp            int64   `json:"sip_timestamp,omitempty"`
+	ParticipantTimestamp int64   `json:"participant_timestamp,omitempty"`
+	Conditions           []int32 `json:"conditions,omitempty"`
+	Price                float64 `json:"price,omitempty"`
+	Size                 uint32  `json:"size,omitempty"`
+	Exchange             int32   `json:"exchange,omitempty"`
+	Timeframe            string  `json:"timeframe,omitempty"`
+	ID                   string  `json:"id,omitempty"`
+	LastUpdated          int64   `json:"last_updated,omitempty"`
 }
 
 // Details contains all the information that might come back in the details attribute of a SnapshotResponse.
