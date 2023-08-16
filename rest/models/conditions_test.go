536fbc98c5507fde520cbda1ddd5edcb0402297d
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-08-16 10:00:57 -0700

    Add tests for some models (recreate #269 w/ updates)

---
+++ rest/models/conditions_test.go
@@ -0,0 +1,36 @@
+package models_test
+
+import (
+	"testing"
+
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func TestListConditionsParams(t *testing.T) {
+	assetClass := models.AssetStocks
+	dataType := models.DataTrade
+	id := int64(1)
+	sip := models.CTA
+	order := models.Asc
+	limit := 20
+	sort := models.Name
+
+	expect := models.ListConditionsParams{
+		AssetClass: &assetClass,
+		DataType:   &dataType,
+		ID:         &id,
+		SIP:        &sip,
+		Order:      &order,
+		Limit:      &limit,
+		Sort:       &sort,
+	}
+	actual := models.ListConditionsParams{}.
+		WithAssetClass(assetClass).
+		WithDataType(dataType).
+		WithID(id).
+		WithSIP(sip).
+		WithOrder(order).
+		WithLimit(limit).
+		WithSort(sort)
+	checkParams(t, expect, *actual)
+}
