80db909cef60d462b559e7bfe3017f92447e3f0f
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-08-22 09:58:36 -0700

    Add tests for some models (recreate #327 resolve github action issue) (#333)
    Co-authored-by: GitHub Action <action@github.com>

---
+++ rest/models/contracts_test.go
@@ -0,0 +1,79 @@
+package models_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func TestGetOptionsContractParams(t *testing.T) {
+	ticker := "A"
+	date := models.Date(time.Date(2023, 3, 23, 0, 0, 0, 0, time.Local))
+	expect := models.GetOptionsContractParams{
+		Ticker: ticker,
+		AsOf:   &date,
+	}
+	actual := models.GetOptionsContractParams{
+		Ticker: ticker,
+	}.WithAsOf(date)
+	checkParams(t, expect, *actual)
+}
+
+func TestListOptionsContractsParams(t *testing.T) {
+	date := models.Date(time.Date(2023, 3, 23, 0, 0, 0, 0, time.Local))
+	contractType := "call"
+	expired := true
+	strike := 100.0
+	sort := models.TickerSymbol
+	order := models.Asc
+	limit := 100
+	ticker := "A"
+	expect := models.ListOptionsContractsParams{
+		ContractType:        &contractType,
+		UnderlyingTickerEQ:  &ticker,
+		UnderlyingTickerLT:  &ticker,
+		UnderlyingTickerLTE: &ticker,
+		UnderlyingTickerGT:  &ticker,
+		UnderlyingTickerGTE: &ticker,
+		ExpirationDateEQ:    &date,
+		ExpirationDateLT:    &date,
+		ExpirationDateLTE:   &date,
+		ExpirationDateGT:    &date,
+		ExpirationDateGTE:   &date,
+		StrikePriceEQ:       &strike,
+		StrikePriceLT:       &strike,
+		StrikePriceLTE:      &strike,
+		StrikePriceGT:       &strike,
+		StrikePriceGTE:      &strike,
+		AsOf:                &date,
+		Expired:             &expired,
+		Sort:                &sort,
+		Order:               &order,
+		Limit:               &limit,
+	}
+	actual := models.ListOptionsContractsParams{}.
+		WithContractType(contractType).
+		WithUnderlyingTicker(models.EQ, ticker).
+		WithUnderlyingTicker(models.LT, ticker).
+		WithUnderlyingTicker(models.LTE, ticker).
+		WithUnderlyingTicker(models.GT, ticker).
+		WithUnderlyingTicker(models.GTE, ticker).
+		WithExpirationDate(models.EQ, date).
+		WithExpirationDate(models.LT, date).
+		WithExpirationDate(models.LTE, date).
+		WithExpirationDate(models.GT, date).
+		WithExpirationDate(models.GTE, date).
+		WithStrikePrice(models.EQ, strike).
+		WithStrikePrice(models.LT, strike).
+		WithStrikePrice(models.LTE, strike).
+		WithStrikePrice(models.GT, strike).
+		WithStrikePrice(models.GTE, strike).
+		WithAsOf(date).
+		WithExpired(expired).
+		WithSort(sort).
+		WithOrder(order).
+		WithLimit(limit)
+
+	checkParams(t, expect, *actual)
+}
