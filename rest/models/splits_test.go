734744a8f952505149ef673195749bd7b2cf293b
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-08-22 15:14:37 -0700

    Added tests for exchanges, indicators, and splits

---
+++ rest/models/splits_test.go
@@ -0,0 +1,51 @@
+package models_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func TestListSplitsParams(t *testing.T) {
+	ticker := "A"
+	date := models.Date(time.Date(2023, 3, 23, 0, 0, 0, 0, time.Local))
+	reverseSplit := true
+	sort := models.TickerSymbol
+	order := models.Asc
+	limit := 100
+
+	expect := models.ListSplitsParams{
+		TickerEQ:         &ticker,
+		TickerLT:         &ticker,
+		TickerLTE:        &ticker,
+		TickerGT:         &ticker,
+		TickerGTE:        &ticker,
+		ExecutionDateEQ:  &date,
+		ExecutionDateLT:  &date,
+		ExecutionDateLTE: &date,
+		ExecutionDateGT:  &date,
+		ExecutionDateGTE: &date,
+		ReverseSplit:     &reverseSplit,
+		Sort:             &sort,
+		Order:            &order,
+		Limit:            &limit,
+	}
+	actual := models.ListSplitsParams{}.
+		WithTicker(models.EQ, ticker).
+		WithTicker(models.LT, ticker).
+		WithTicker(models.LTE, ticker).
+		WithTicker(models.GT, ticker).
+		WithTicker(models.GTE, ticker).
+		WithExecutionDate(models.EQ, date).
+		WithExecutionDate(models.LT, date).
+		WithExecutionDate(models.LTE, date).
+		WithExecutionDate(models.GT, date).
+		WithExecutionDate(models.GTE, date).
+		WithReverseSplit(reverseSplit).
+		WithSort(sort).
+		WithOrder(order).
+		WithLimit(limit)
+
+	checkParams(t, expect, *actual)
+}
