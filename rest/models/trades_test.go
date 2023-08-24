d3b2a9ba58037a30102cb0cda96e3ec5f228760a
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-08-24 08:42:12 -0700

    Added tests for dividends, financials, trades, quotes, and tickers (recreate of 335)

---
+++ rest/models/trades_test.go
@@ -0,0 +1,37 @@
+package models_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func TestListTradesParams(t *testing.T) {
+	timestamp := models.Nanos(time.Date(2023, 3, 23, 0, 0, 0, 0, time.UTC))
+	order := models.Asc
+	limit := 100
+	sort := models.Timestamp
+
+	expect := models.ListTradesParams{
+		TimestampEQ:  &timestamp,
+		TimestampLT:  &timestamp,
+		TimestampLTE: &timestamp,
+		TimestampGT:  &timestamp,
+		TimestampGTE: &timestamp,
+		Order:        &order,
+		Limit:        &limit,
+		Sort:         &sort,
+	}
+	actual := models.ListTradesParams{}.
+		WithTimestamp(models.EQ, timestamp).
+		WithTimestamp(models.LT, timestamp).
+		WithTimestamp(models.LTE, timestamp).
+		WithTimestamp(models.GT, timestamp).
+		WithTimestamp(models.GTE, timestamp).
+		WithOrder(order).
+		WithLimit(limit).
+		WithSort(sort)
+
+	checkParams(t, expect, *actual)
+}
