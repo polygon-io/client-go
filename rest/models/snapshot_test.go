abc3bc4541bea5e89622db77eb28691220291560
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-08-25 10:09:07 -0700

    Added tests for models snapshots

---
+++ rest/models/snapshot_test.go
@@ -0,0 +1,91 @@
+package models_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func TestGetAllTickersSnapshotParams(t *testing.T) {
+	tickers := "AAPL,GOOL,TSLA"
+	otc := false
+	expect := models.GetAllTickersSnapshotParams{
+		Tickers:    &tickers,
+		IncludeOTC: &otc,
+	}
+	actual := models.GetAllTickersSnapshotParams{}.
+		WithTickers(tickers).
+		WithIncludeOTC(otc)
+
+	checkParams(t, expect, *actual)
+}
+
+func TestGetIndicesSnapshotParams(t *testing.T) {
+	tickers := "AAPL,GOOL,TSLA"
+	expect := models.GetIndicesSnapshotParams{
+		TickerAnyOf: &tickers,
+	}
+	actual := models.GetIndicesSnapshotParams{}.WithTickerAnyOf(tickers)
+	checkParams(t, expect, *actual)
+}
+
+func TestListUniversalSnapshotsParams(t *testing.T) {
+	ticker := "A"
+	tickers := "AAPL,GOOL,TSLA"
+	snapshot := "stocks"
+
+	expect := models.ListUniversalSnapshotsParams{
+		TickerAnyOf: &tickers,
+		Ticker:      &ticker,
+		TickerLT:    &ticker,
+		TickerLTE:   &ticker,
+		TickerGT:    &ticker,
+		TickerGTE:   &ticker,
+		Type:        &snapshot,
+	}
+	actual := models.ListUniversalSnapshotsParams{}.
+		WithTickerAnyOf(tickers).
+		WithTicker(ticker).
+		WithTickersByComparison(models.LT, ticker).
+		WithTickersByComparison(models.LTE, ticker).
+		WithTickersByComparison(models.GT, ticker).
+		WithTickersByComparison(models.GTE, ticker).
+		WithType(snapshot)
+
+	checkParams(t, expect, *actual)
+}
+
+func TestListOptionsChainParams(t *testing.T) {
+	date := models.Date(time.Date(2023, 3, 23, 0, 0, 0, 0, time.Local))
+	contractType := models.ContractCall
+	strikePrice := 1.23
+	limit := 100
+	sort := models.TickerSymbol
+	order := models.Asc
+	expect := models.ListOptionsChainParams{
+		StrikePrice:       &strikePrice,
+		ContractType:      &contractType,
+		ExpirationDateEQ:  &date,
+		ExpirationDateLT:  &date,
+		ExpirationDateLTE: &date,
+		ExpirationDateGT:  &date,
+		ExpirationDateGTE: &date,
+		Limit:             &limit,
+		Sort:              &sort,
+		Order:             &order,
+	}
+	actual := models.ListOptionsChainParams{}.
+		WithStrikePrice(strikePrice).
+		WithContractType(contractType).
+		WithExpirationDate(models.EQ, date).
+		WithExpirationDate(models.LT, date).
+		WithExpirationDate(models.LTE, date).
+		WithExpirationDate(models.GT, date).
+		WithExpirationDate(models.GTE, date).
+		WithLimit(limit).
+		WithSort(sort).
+		WithOrder(order)
+
+	checkParams(t, expect, *actual)
+}
