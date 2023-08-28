5cabc072160e2e746f487f5b3dbdf30f57495292
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-08-28 08:43:38 -0700

    Added test for summaries model (#343)
    Co-authored-by: GitHub Action <action@github.com>

---
+++ rest/models/summaries_test.go
@@ -0,0 +1,16 @@
+package models_test
+
+import (
+	"testing"
+
+	"github.com/polygon-io/client-go/rest/models"
+)
+
+func TestGetSummaryParams(t *testing.T) {
+	tickers := "AAPL,GOOL,TSLA"
+	expect := models.GetSummaryParams{
+		TickerAnyOf: &tickers,
+	}
+	actual := models.GetSummaryParams{}.WithTickerAnyOf(tickers)
+	checkParams(t, expect, *actual)
+}
