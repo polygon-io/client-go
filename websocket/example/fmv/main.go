546898352766b99eb741645bde4b7dd179ba8321
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-12-18 10:37:19 -0800

    Added FMV messages to output channel

---
+++ websocket/example/fmv/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+
+	polygonws "github.com/polygon-io/client-go/websocket"
+	"github.com/polygon-io/client-go/websocket/models"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	log := logrus.New()
+	log.SetLevel(logrus.DebugLevel)
+	log.SetFormatter(&logrus.JSONFormatter{})
+	c, err := polygonws.New(polygonws.Config{
+		APIKey: os.Getenv("POLYGON_API_KEY"),
+		Feed:   polygonws.BusinessFeed,
+		Market: polygonws.Stocks,
+		Log:    log,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	// FMV
+	_ = c.Subscribe(polygonws.BusinessFairMarketValue, "*")
+
+	if err := c.Connect(); err != nil {
+		log.Error(err)
+		return
+	}
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+
+	for {
+		select {
+		case <-sigint:
+			return
+		case <-c.Error():
+			return
+		case out, more := <-c.Output():
+			if !more {
+				return
+			}
+			switch out.(type) {
+			case models.FairMarketValue:
+				log.WithFields(logrus.Fields{"fmv": out}).Info()
+			default:
+				log.WithFields(logrus.Fields{"unknown": out}).Info()
+			}
+		}
+	}
+}
